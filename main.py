3b16608ee1ab8c7dc1d498fff30ab22a9fb8915d
Tautminas Cibulskis <tautminas@yahoo.com>
2023-09-20 16:48:45 +0300

    Get the whole table and covert it to dataframe

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
+from io import StringIO
+import lxml
 
-response = requests.get("https://finance.yahoo.com/losers")
+response = requests.get("https://finance.yahoo.com/losers?count=100")
 
 yahoo_finance_web_page = response.text
 print(f"Response status code: {response.status_code}")
 
 soup = BeautifulSoup(yahoo_finance_web_page, 'html.parser')
-print(f"Title of the page: {soup.title}")
 
-biggest_loser = soup.select_one("#scr-res-table > div.Ovx\(a\).Ovx\(h\)--print.Ovy\(h\).W\(100\%\) > table > tbody > tr:nth-child(1) > td.Va\(m\).Ta\(start\).Pstart\(6px\).Pend\(10px\).Miw\(90px\).Start\(0\).Pend\(10px\).simpTblRow\:h_Bgc\(\$hoverBgColor\).Pos\(st\).Bgc\(\$lv3BgColor\).Z\(1\).Bgc\(\$lv2BgColor\).Ta\(start\)\!.Fz\(s\) > a")
-biggest_loser_change_percent = soup.select_one("#scr-res-table > div.Ovx\(a\).Ovx\(h\)--print.Ovy\(h\).W\(100\%\) > table > tbody > tr:nth-child(1) > td:nth-child(5) > fin-streamer > span")
-print(f"Biggest loser is {biggest_loser['title']} with {biggest_loser_change_percent.text} change loss.")
+losers_table = soup.select_one("#scr-res-table table")
+df_losers = pd.read_html(StringIO(str(losers_table)))[0]
+df_losers['% Change'] = pd.to_numeric(df_losers['% Change'].str.replace('%', ''), errors='coerce')
+# Filtering
+df_losers = df_losers[df_losers['% Change'] < -10]
+
+print(df_losers)
+
+for index, row in df_losers.iterrows():
+    print(row['Symbol'], row['% Change'])

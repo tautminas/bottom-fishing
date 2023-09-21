eae984638216f948ed6168a48ac3b9c4d22e82d3
Tautminas Cibulskis <tautminas@yahoo.com>
2023-09-21 17:16:21 +0300

    Write a function for price book value

---
+++ FUTURE_IMPROVEMENTS.md
@@ -0,0 +1,12 @@
+Iterate through df. Get stocks:
+1) With at least 5 percent change within the day
+2) The Price-to-Book (P/B) ratio is a financial metric less than one (https://finance.yahoo.com/quote/GETY/analysis?p=GETY)
+3) Recommendation rating is less than 2 (https://finance.yahoo.com/quote/GETY/analysis?p=GETY)
+
+Get the 3 main stocks. 
+
+Add some news about each of them via API.
+
+Send emails and SMS. 10 minutes before the market is opened.
+
+Add parameters to the app via argparse with help so there would be options.
--- main.py
+++ main.py
@@ -4,20 +4,42 @@ import pandas as pd
 from io import StringIO
 import lxml
 
-response = requests.get("https://finance.yahoo.com/losers?count=100")
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/58.0.3029.110 Safari/537.36"
+}
 
-yahoo_finance_web_page = response.text
-print(f"Response status code: {response.status_code}")
 
-soup = BeautifulSoup(yahoo_finance_web_page, 'html.parser')
+def get_stocks_losers():
+    response = requests.get("https://finance.yahoo.com/losers?count=100", headers=HEADERS)
+    yahoo_finance_web_page = response.text
+    if str(response.status_code)[0] != "2":
+        return None
+    finance_soup = BeautifulSoup(yahoo_finance_web_page, 'html.parser')
+    losers_table = finance_soup.select_one("#scr-res-table table")
+    df = pd.read_html(StringIO(str(losers_table)))[0]
+    df['% Change'] = pd.to_numeric(df['% Change'].str.replace('%', ''), errors='coerce')
+    columns_to_drop = ['Change', 'Volume', 'Avg Vol (3 month)', 'Market Cap', 'PE Ratio (TTM)', '52 Week Range']
+    df = df.drop(columns=columns_to_drop)
+    df = df[df['% Change'] < -5]
+    return df
 
-losers_table = soup.select_one("#scr-res-table table")
-df_losers = pd.read_html(StringIO(str(losers_table)))[0]
-df_losers['% Change'] = pd.to_numeric(df_losers['% Change'].str.replace('%', ''), errors='coerce')
-# Filtering
-df_losers = df_losers[df_losers['% Change'] < -10]
 
-print(df_losers)
+def get_price_book(symbol):
+    url = f"https://finance.yahoo.com/quote/{symbol}/key-statistics?p={symbol}"
+    response = requests.get(url, headers=HEADERS)
+    if str(response.status_code)[0] != "2":
+        return None
+    stats_page = response.text
+    stats_soup = BeautifulSoup(stats_page, 'html.parser')
+    price_book = stats_soup.select_one("div:nth-child(1) > div > div > div > div > table > tbody > tr:nth-child(7)")
+    price_book = float(price_book.get_text().replace("Price/Book (mrq)", ""))
+    print(price_book)
+    return price_book
 
-for index, row in df_losers.iterrows():
-    print(row['Symbol'], row['% Change'])
+
+if __name__ == "__main__":
+    df_losers = get_stocks_losers()
+    pb = get_price_book(df_losers['Symbol'][0])
+    print(df_losers)
+    print(pb)
